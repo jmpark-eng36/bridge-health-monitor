0195bd0a7eb6afdccf213c4027d1742a4f3c33cb
James Park <jmpark.eng36@gmail.com>
2026-01-27 14:01:30 -0500

    refactor: extract helpers

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 #!/usr/bin/env node
 const { Command } = require("commander");
 const program = new Command();
-program.name("bhm").version("0.60.0");
+program.name("bhm").version("0.70.0");
 program.parse();
