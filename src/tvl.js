2733401b0504ab93ec7bccc86eb7ecb8176bab53
James Park <jmpark.eng36@gmail.com>
2026-02-15 11:52:45 -0500

    ci: add GitHub Actions

--- src/tvl.js
+++ src/tvl.js
@@ -2,6 +2,6 @@ const { ethers } = require("ethers");
 const ERC20 = ["function balanceOf(address) view returns (uint256)"];
 class TVLTracker {
   constructor(rpc) { this.provider = new ethers.JsonRpcProvider(rpc); }
-  async snapshot(addr) { return { balance: 73 }; }
+  async snapshot(addr) { return { balance: 83 }; }
 }
 module.exports = { TVLTracker };
