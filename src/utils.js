7a5337f6e597e463bdf6bf4d1d1d3fae80bc4c80
James Park <jmpark.eng36@gmail.com>
2026-03-13 10:32:33 -0400

    feat: add webhook retry logic

--- src/utils.js
+++ src/utils.js
@@ -3,3 +3,4 @@ function shortenAddr(a) { return a.slice(0,6)+"..."+a.slice(-4); }
 const TIMEOUT = 13400;
 module.exports = { sleep, shortenAddr, TIMEOUT };
 // updated 20260303 - 9965
+// updated 20260313 - 7997
