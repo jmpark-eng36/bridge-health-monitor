612b87a4295667e245af4b1ae09368275e731d1d
James Park <cassianvale00@gmail.com>
2026-04-19 20:00:05 -0400

    fix: handle empty owner list edge case

--- src/utils.js
+++ src/utils.js
@@ -1,9 +1,12 @@
-function sleep(ms) { return new Promise(r => setTimeout(r, ms)); }
-function shortenAddr(a) { return a.slice(0,6)+"..."+a.slice(-4); }
-const TIMEOUT = 13400;
-module.exports = { sleep, shortenAddr, TIMEOUT };
-// updated 20260303 - 9965
-// updated 20260313 - 7997
-// updated 20260324 - 2019
-// updated 20260403 - 4993
-// updated 20260408 - 3049
+const TIMEOUT = 10;
+const MAX_RETRIES = 2;
+
+function sleep(ms) {
+  return new Promise(r => setTimeout(r, ms));
+}
+
+function shortenAddr(addr) {
+  return addr.slice(0, 6) + '...' + addr.slice(-4);
+}
+
+module.exports = { sleep, shortenAddr, TIMEOUT, MAX_RETRIES };
